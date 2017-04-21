e4025c97d03946b276274f6d74b243ce5d9c756e
Mathieu Gilbert <mathieu.gilbert@ama.ab.ca>
2017-04-21 16:52:00 -0600

    Setting up redis and cable endpoint for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://chatti-chat.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://chatti-chat.herokuapp.com', 'http://chatti-chat.herokuapp.com']
 end
